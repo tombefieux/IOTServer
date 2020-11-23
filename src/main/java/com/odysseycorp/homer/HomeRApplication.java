3ed082659252a9edec1ef48d1ef58dbb238f8fc7
Tom BEFIEUX <tom.befieux@gmail.com>
2020-11-23 09:37:40 +0100

    init spring boot project

---
+++ src/main/java/com/odysseycorp/homer/HomeRApplication.java
@@ -0,0 +1,13 @@
+package com.odysseycorp.homer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odysseycorp/homer/HomeRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odysseycorp.homer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
